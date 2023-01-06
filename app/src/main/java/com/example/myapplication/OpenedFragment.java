5bb3d497cdc5a719e50972169981fab8f7941d19
ann7887 <anyta.rodenkova@gmail.com>
2023-01-06 21:13:28 +0300

    Commit2

--- app/src/main/java/com/example/myapplication/OpenedFragment.java
+++ app/src/main/java/com/example/myapplication/OpenedFragment.java
@@ -9,11 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OpenedFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OpenedFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -29,15 +24,6 @@ public class OpenedFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OpenedFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static OpenedFragment newInstance(String param1, String param2) {
         OpenedFragment fragment = new OpenedFragment();
         Bundle args = new Bundle();
--- app/src/main/java/com/example/myapplication/RootFragment.java
+++ app/src/main/java/com/example/myapplication/RootFragment.java
@@ -19,11 +19,6 @@ import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RootFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RootFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -31,7 +26,6 @@ public class RootFragment extends Fragment {
     private static final String ARG_PARAM1 = "SUCCESS";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -39,15 +33,6 @@ public class RootFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RootFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RootFragment newInstance(String param1, String param2) {
         RootFragment fragment = new RootFragment();
         Bundle args = new Bundle();
@@ -70,70 +55,54 @@ public class RootFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_root, container, false);
         final EditText passwordEditText = view.findViewById(R.id.editTextTextPassword2);
+        final EditText usernameEditText = view.findViewById(R.id.editTextUsername);
+        final TextInputLayout usernameTextInput = view.findViewById(R.id.username_text_input);
         final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
         Button btn = view.findViewById(R.id.next_button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!isPasswordValid(passwordEditText.getText())) {
-                    passwordTextInput.setError(getString(R.string.shr_error_password));
-                } else {
-                    passwordTextInput.setError(null);
-
-                    Navigation.findNavController(view).navigate(R.id.action_rootFragment_to_openedFragment);
+                if (!isUsernameValid(usernameEditText.getText())) {
+                    usernameTextInput.setError(getString(R.string.shr_error_username));
                 }
+                else if (!isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(getString(R.string.shr_error_password));
+                }
+                else {
+                    passwordTextInput.setError(null);
+                    usernameTextInput.setError(null);
+                    Bundle bundle = new Bundle();
+                    bundle.putString(ARG_PARAM1, "correct password");
+                    Navigation.findNavController(view).navigate(R.id.action_rootFragment_to_openedFragment, bundle);
+                }
+            }
+        });
+        usernameEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int j, KeyEvent keyEvent1) {
+                if (isUsernameValid(usernameEditText.getText())) {
+                    usernameTextInput.setError(null);
+                }
+                return false;
             }
         });
         passwordEditText.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+            public boolean onKey(View view, int i, KeyEvent keyEvent2) {
                 if (isPasswordValid(passwordEditText.getText())) {
-                    passwordTextInput.setError(null); //Clear the error
+                    passwordTextInput.setError(null);
                 }
                 return false;
             }
         });
         return view;
+
     }
-/*
-    @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_root, container, false);
-        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
-        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
-        Button nextButton = view.findViewById(R.id.next_button);
 
-        // Set an error if the password is less than 8 characters.
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Navigation.findNavController(view).navigate(R.id.action_rootFragment_to_openedFragment);
-                if (!isPasswordValid(passwordEditText.getText())) {
-                    passwordTextInput.setError(getString(R.string.shr_error_password));
-                } else {
-                    passwordTextInput.setError(null);
-                    Navigation.findNavController(view).navigate(R.id.action_rootFragment_to_openedFragment);
-                }
-            }
-        });
-        return view;
-
-        // Clear the error once more than 8 characters are typed.
-        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                if (isPasswordValid(passwordEditText.getText())) {
-                    passwordTextInput.setError(null); //Clear the error
-                }
-                return false;
-            }
-        });
-    }
-*/
     private boolean isPasswordValid(@Nullable Editable text) {
         return text != null && text.length() >= 8;
+    }
+    private boolean isUsernameValid(@Nullable Editable textt) {
+        return textt != null && textt.length() >= 1;
     }
 }
